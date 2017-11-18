04c3e6606cd1989ee02d87ba10b18ddfa3f8ebac
Aram <avalbaro@gmail.com>
2017-11-18 11:04:54 -0800

    update to code

---
+++ server.js
@@ -0,0 +1,12 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+ar app = express();
+var PORT = process.env.port || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
